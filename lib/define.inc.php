31124e81cb043f502cc3d709c8e5956a522598c6
felipejorgeas <felipejorgeas@gmail.com>
2014-04-23 17:56:34 -0300

    Atualizado para a nova versao.

--- lib/define.inc.php
+++ lib/define.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 // versao do sistema
-define('VERSAO', '0.5');
+define('VERSAO', '0.6');
 
 // Cargo do Funcionario 'empfnc.type' ON (emp.funcao = empfnc.no)
 define('EMPTYPE_INDEFINIDO',  0);
